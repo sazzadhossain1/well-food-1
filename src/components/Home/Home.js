6c0fe1dc33d37eece1b08626b763332bb8125724
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-04-04 04:15:10 +0600

    create fake api

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,22 +4,30 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="home-component">
-      <div className="details-div">
-        <p className="delivery">Free Home Delivery 24 Hours</p>
-        <h1>THE DELIGHT BURGER </h1>
-        <h2>Your best Food</h2>
-        <h5>
-          Sink your teeth into a delicious restaurant-style, hamburger recipe
-          made from lean beef. Skip the prepackaged patties and take the extra
-          time to craft up your own, and that little extra effort will be worth
-          it. To make cheeseburgers, about 1 minute before burgers are done, top
-          with sliced cheese; continue cooking until cheese begins to melt.
-        </h5>
+    <div>
+      <div className="home-component">
+        <div className="details-div">
+          <p className="delivery">Free Home Delivery 24 Hours</p>
+          <h1>THE DELIGHT BURGER </h1>
+          <h2>Your best Food</h2>
+          <h5>
+            Sink your teeth into a delicious restaurant-style, hamburger recipe
+            made from lean beef. Skip the prepackaged patties and take the extra
+            time to craft up your own, and that little extra effort will be
+            worth it. To make cheeseburgers, about 1 minute before burgers are
+            done, top with sliced cheese; continue cooking until cheese begins
+            to melt.
+          </h5>
+        </div>
+
+        <div>
+          <img src={img} alt="" />
+        </div>
       </div>
 
-      <div>
-        <img src={img} alt="" />
+
+      <div className="customer-reviews">
+          <h1>Customer Reviews</h1>
       </div>
     </div>
   );
